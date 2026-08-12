2f886bd33e804f95068fc84fcbdbe2bf2950d94d
Роман Какорин <rkakorin@ecom.tech>
2026-08-12 13:30:49 +0200

    Init gem

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2026-08-12
+
+- Initial release
---
+++ lib/transactional_outbox.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "transactional_outbox/version"
+
+module TransactionalOutbox
+  class Error < StandardError; end
+  # Your code goes here...
+end

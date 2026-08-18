bf334da42d4290c0d1b02f770508c338e014c5af
Roman Kakorin <romchky1@gmail.com>
2026-08-18 14:09:13 +0200

    [IS-4] Added context and payload validations

---
+++ lib/transactional_outbox/event/contextable.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module TransactionalOutbox
+  class Event
+    module Contextable
+      attr_reader :context_schema
+
+      def self.extended(klass)
+        klass.define_method(:validate_context!) do |params|
+          ctx_schema = self.class.instance_variable_get(:@__context_schema__)
+
+          return unless ctx_schema
+
+          result = ctx_schema.call(params)
+
+          self.class.check_context_validation_result!(params, result)
+        end
+      end
+
+      def check_context_validation_result!(params, result)
+        return if result.success?
+
+        errors = result.message_set.messages.map do |message|
+          "#{message.path.join("->")} #{message.text}"
+        end.join(", ")
+
+        "Invalid context error: #{self.class.stringify_errors(result)}, context: #{params}"
+      end
+
+      def context(&block)
+        @__context_schema__ ||= Class.new(Dry::Validation::Contract).class_exec { params(&block) } if block
+      end
+    end
+  end
+end

aba830495ff84dac6b09e01b85b6c7b42c978850
Roman Kakorin <romchky1@gmail.com>
2026-08-18 14:14:24 +0200

    [IS-4] Fix config args

--- lib/transactional_outbox/event/message_builder.rb
+++ lib/transactional_outbox/event/message_builder.rb
@@ -3,10 +3,8 @@
 module TransactionalOutbox
   class Event
     class MessageBuilder
-      def self.build(event, payload, *_context)
-        config = event.class.config
-
-        { topic: config.topic, event_type: config.event_type.to_s, payload: }
+      def self.build(event_config, payload, *_context)
+        { topic: event_config.topic, event_type: event_config.event_type.to_s, payload: }
       end
     end
   end

86cc75ff735e407836ec4502ddc9d81f629b6ab0
Roman Kakorin <romchky1@gmail.com>
2026-08-18 14:11:34 +0200

    [IS-4] Hide dataset for null

--- lib/transactional_outbox/database/adapters/null.rb
+++ lib/transactional_outbox/database/adapters/null.rb
@@ -5,8 +5,8 @@ module TransactionalOutbox
     class Adapters
       class Null < Interface
         class << self
-          def dataset = @dataset ||= {}
-          def clear_store = @dataset = {}
+          def dataset = @__dataset__ ||= {}
+          def clear_store = @__dataset__ = {}
         end
 
         def dataset = self.class.dataset[table] ||= []

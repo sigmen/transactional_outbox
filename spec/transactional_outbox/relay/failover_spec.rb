f1fd412d362c601581550dcf01017048906871fe
Роман Какорин <rkakorin@ecom.tech>
2026-08-20 17:41:30 +0200

    IS-5 Rename worker set

--- spec/transactional_outbox/relay/failover_spec.rb
+++ spec/transactional_outbox/relay/failover_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TransactionalOutbox::Relay::Failover do
   subject(:failover) { described_class.call(error, worker) }
 
   let(:error) { StandardError.new }
-  let(:worker) { TransactionalOutbox::Relay::WorkersSet::Worker.new("test-topic") }
+  let(:worker) { TransactionalOutbox::Relay::WorkerSet::Worker.new("test-topic") }
 
   it "raises an error" do
     expect { failover }.to raise_error error

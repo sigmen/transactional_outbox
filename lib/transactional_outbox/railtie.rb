94f224b528a1901021c33af74743e90ce6cd79fd
Roman Kakorin <romchky1@gmail.com>
2026-08-21 17:50:56 +0200

    IS-5 Added relay rake task

--- lib/transactional_outbox/railtie.rb
+++ lib/transactional_outbox/railtie.rb
@@ -5,5 +5,9 @@ module TransactionalOutbox
     generators do
       require_relative "../generators/transactional_outbox/migration/migration_generator.rb"
     end
+
+    rake_tasks do
+      Dir[File.join(File.dirname(__FILE__), "tasks/*.rake")].each { |f| load f }
+    end
   end
 end
